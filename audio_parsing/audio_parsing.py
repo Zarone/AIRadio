went back to fixed normalization for better standardization

audio_parsing/audio_parsing.py
@@ -8,8 +8,9 @@ import random
 def get_raw_data() -> Tuple[np.ndarray, List]:
   dir = get_directories()
   song_names = [song_dir for song_dir in dir]
-  print("REMINDER: I shrunk that song size to three element")
-  song_data = np.array([get_raw_audio(song_dir)[0:100] for song_dir in dir])
+  max_elements = 100
+  print(f"REMINDER: I shrunk that song size to {max_elements} element")
+  song_data = np.array([get_raw_audio(song_dir)[0:max_elements] for song_dir in dir])
   return (song_data, song_names)
 
 def play_audio(audio_data) -> None:

main.py
@@ -21,28 +21,31 @@ sounds, names = audio.get_raw_data()
 song_length = len(sounds[0])
 print(f"Current Size of Each Song: {len(sounds[0])}")
 
-encoder_layers = (song_length, 500, 500, 10)
+# Prevents loss gradients from being too
+# extreme
+sounds = sounds/10
+
+encoder_layers = (song_length, 50, 25)
 decoder_layers = encoder_layers[::-1]
  
 compression_VAE = VAE(encoder_layers, decoder_layers)
- 
-compression_VAE.train(sounds, 1000, len(sounds), graph=True, learning_rate=0.05)
+compression_VAE.train(sounds, 5000, len(sounds), graph=True, learning_rate=0.05)
 
-print(f"test input:\n {sounds[0]}")
+print(f"test input:\n {sounds[0]*song_length}")
 
 _, _, mu, log_variance = compression_VAE.encode( sounds[0] )
 
-print(f"mu:\n {mu} \nsigma:\n {np.exp(0.5*log_variance)}")
+# print(f"mu:\n {mu} \nsigma:\n {np.exp(0.5*log_variance)}")
 
-generated, epsilon = compression_VAE.gen(mu, log_variance)
+generated, epsilon = compression_VAE.gen(mu, -100)
 
-print(f"generated:\n {generated}")
+# print(f"generated:\n {generated}")
 
 decoded = compression_VAE.decode(generated)[1]
 
-print(f"decoded:\n {decoded[-1]}")
+print(f"decoded:\n {decoded[-1]*song_length}")
 
-print(f"off by:\n {decoded[-1]-sounds[0]}")
+print(f"off by:\n {song_length*(decoded[-1]-sounds[0])}")
 
 # compressed_sounds = np.asarray([compression_VAE.encode(sound) for sound in sounds])
 

neural_networks/vae/normal_vae/normal_vae.py
@@ -149,7 +149,7 @@ class VAE(BaseNetwork):
     return (z_values, activations)
 
   def gen(self, mu, log_variance, iter=100) -> Tuple[np.ndarray, np.ndarray]:
-    a = 0.01
+    a = 0
     epsilon = (np.random.randn(len(mu)).reshape(-1, 1)) * (1-math.exp(-a*iter))
     z = mu + np.exp(0.5 * log_variance) * epsilon
     return (z, epsilon)
@@ -206,8 +206,11 @@ class VAE(BaseNetwork):
     bias_gradient = np.empty(self.biases.shape, np.ndarray)
     reconstruction_loss = 0
     kl_loss = 0
-    total_datapoint_magnitude = 0.001 + np.linalg.norm(batch) 
-    Beta = 1
+
+    # Beta affects the relative importance of kl_loss 
+    # with respect to reconstruction_loss in calculating
+    # the gradient.
+    Beta = 0
 
     for i, _ in enumerate(weight_gradient):
       weight_gradient[i] = np.zeros(self.weights[i].shape)
@@ -291,7 +294,7 @@ class VAE(BaseNetwork):
       weight_gradient[0] += np.matmul(decoder_gradients_z[0], data_point.transpose())
       bias_gradient[0] += decoder_gradients_z[0]
 
-    self.weights -= learning_rate * weight_gradient/total_datapoint_magnitude/len(batch)
+    self.weights -= learning_rate * weight_gradient/len(batch)
     self.biases -= learning_rate * bias_gradient/len(batch)
     return reconstruction_loss/len(batch), kl_loss/len(batch)
 