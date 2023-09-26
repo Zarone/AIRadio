84ad47de13004b7d65588d676c70c2fd9d9584c6
Zachary Alfano <zacharyalfano2@gmail.com>
2023-09-26 09:45:38 -0400

    no state layer

--- neural_networks/vae/vae_test.py
+++ neural_networks/vae/vae_test.py
@@ -45,13 +45,11 @@ def vae_test(Tester):
             encoder_args=dict(
                 input_size=5,
                 input_layers=(9, 4),
-                state_layers=(4, 4),
                 output_layers=(4, 6)
             ),
             decoder_args=dict(
                 input_size=3,
                 input_layers=(7, 4),
-                state_layers=(4, 4),
                 output_layers=(4, 5)
             ),
             latent_size=3,
